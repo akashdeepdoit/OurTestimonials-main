0a9d10aa013fbb5fc2e89788545334f1d1d8199d
akashdeepdoit <akashdeep.doit@gmail.com>
2024-01-05 23:14:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const App = () => {
+  return (
+     <div>
+      <div>
+        <h1>Our Testimonials</h1>
+        <div></div>
+        <Testimonial/>
+      </div>
+
+     </div>
+  );
+};
+
+export default App;
